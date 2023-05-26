0c3a3b396c8f9bfd1130708d1699b853df48e23f
med00723 <mohamedalouane.ess@gmail.com>
2023-05-26 18:45:50 +0100

    test.st

--- msign/backend/test.sh
+++ msign/backend/test.sh
@@ -29,3 +29,7 @@ echo 'runner Jest (https://facebook.github.io/jest/).'
 echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
 echo 'runner Jest (https://facebook.github.io/jest/).'
+
+echo 'The following "npm" command tests that your simple Node.js/React'
+echo 'application renders satisfactorily. This command actually invokes the test'
+echo 'runner Jest (https://facebook.github.io/jest/).'
