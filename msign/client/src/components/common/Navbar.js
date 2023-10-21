5170fb4413338ea06abed3770303129d137a2699
med <mohamedalouane__ess@gmail.com>
2023-10-21 11:11:11 +0100

    new

--- msign/client/src/components/common/Navbar.js
+++ msign/client/src/components/common/Navbar.js
@@ -27,7 +27,7 @@ const Navbar = () => {
 
         <div class="container">
 
-            <div class="masthead-subheading">Bienvenue sur notre application. test </div>
+            <div class="masthead-subheading">Bienvenue sur notre application. </div>
 
         </div>
     </header>
